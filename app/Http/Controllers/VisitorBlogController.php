3a4e161c7b3eb9083b52aad1d3853533a8c57db8
Aidan Laycock <aidanl_@hotmail.co.uk>
2023-01-24 21:38:36 +0000

    Added ordering within visitor blog posts and loading categories after route model binding for show

--- app/Http/Controllers/VisitorBlogController.php
+++ app/Http/Controllers/VisitorBlogController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\BlogPost;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -20,7 +21,9 @@ class VisitorBlogController extends Controller
             'posts' => BlogPost::select(['id', 'title', 'slug', 'publish_at', 'status', 'created_at', 'updated_at'])
                 ->with('categories')
                 ->published()
+                ->orderBy('created_at', 'DESC')
                 ->get(),
+            'categories' => Category::active()->get(),
             'canLogin' => Route::has('login'),
             'canRegister' => Route::has('register'),
         ]);
@@ -34,6 +37,8 @@ class VisitorBlogController extends Controller
      */
     public function show(BlogPost $post)
     {
+        $post->load('categories');
+
         return Inertia::render('visitor/blog/show', [
             'post' => $post,
             'canLogin' => Route::has('login'),
